271fb4595aacfb3f09138d312d98783d9e66f361
Luis de la Rosa <deluisjaviel7@gmail.com>
2026-09-15 17:27:32 -0400

    Ajusta separación de botones de navegación

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -79,7 +79,7 @@ export default function Navbar() {
         </ul>
 
         {/* Botones */}
-        <div className="ml-auto flex items-center gap-4">
+        <div className="ml-auto flex items-center gap-2">
           <LanguageToggle />
           <ThemeToggle />
         </div>
